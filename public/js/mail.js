5e6ba7962089adeca4b387f8eb3476896d08dcf4
Julian <julisanchez1991@gmail.com>
2020-01-10 23:18:47 -0300

    10/1/2020 -3

--- public/js/mail.js
+++ public/js/mail.js
@@ -3,7 +3,7 @@ const mailGun = require("nodemailer-mailgun-transport");
 
 const auth = {
     auth: {
-        api_key: '548a652f263dd0d46f87663feae3b845-713d4f73-44d8b8a9',
+        api_key: 'key-aac89e90ebb8f3b20427646fef8868f5',
         domain: 'sandboxe2362986e95f472ab2a47f51cc23bc49.mailgun.org'
     }
 };
