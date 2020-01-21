168f2b72c4a62064d342bff95d8d03a67241e8cf
Julian <julisanchez1991@gmail.com>
2020-01-21 17:00:17 -0300

    21/1/2020

--- public/js/samanco-anim.js
+++ public/js/samanco-anim.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
-    console.log("hola");
-    
-    
-    
-    $(".boton").click(function(){
-        $(".tarjeta #parrafo-samanco").slideToggle("slow");
+    console.log($(".tarjeta #parrafo-samanco").css("display"));
 
-    })
+    
+    $(".boton #btn-up").css("display","none");
+    $(".boton").click(function(){
+        if($(".tarjeta #parrafo-samanco").css("display") == "none"){
+        
+            $(".tarjeta #parrafo-samanco").slideDown("slow");
+            $(".boton #btn-down").css("display","none");
+            $(".boton #btn-up").css("display","block");
+            console.log($(".tarjeta #parrafo-samanco").css("display"));
+        
+        }else if ($(".tarjeta #parrafo-samanco").css("display") == "block"){
+            console.log("works");
+            $(".tarjeta #parrafo-samanco").slideUp("slow");
+            $(".boton #btn-down").css("display","block");
+            $(".boton #btn-up").css("display","none");
+        }
+    });
+    
+    
     
 })
