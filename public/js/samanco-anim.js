12657b039474e89e631c576f211d5ccea46a4d3c
Julian <julisanchez1991@gmail.com>
2020-01-23 17:55:14 -0300

    23/1/2020

--- public/js/samanco-anim.js
+++ public/js/samanco-anim.js
@@ -18,7 +18,42 @@ $(document).ready(function(){
             $(".boton #btn-up").css("display","none");
         }
     });
-    
+
+    $(".arrows .fa-angle-up").css("display","none");
+    $(".arrows").click(function(){
+        if($(".helado-info-body p").css("display") == "none"){
+        
+            $(".helado-info-body p").slideDown("slow");
+            $(".helado-info").animate({
+                
+                width: '90%'
+            },"slow");
+            $(".helado-info-body p").animate({
+                "font-size": "14px"
+            },"slow");
+            $(".helado-info img").animate({
+                "margin-top": "0px"
+            },"slow");
+            $(".arrows .fa-angle-down").css("display","none");
+            $(".arrows .fa-angle-up").css("display","block");
+            
+        
+        }else if ($(".helado-info-body p").css("display") == "block"){
+            console.log("works");
+            $(".helado-info-body p").slideUp("slow");
+            $(".helado-info").animate({
+                width: '40%'
+            },"slow");
+            $(".helado-info-body p").animate({
+                "font-size": "0px"
+            },"slow");
+            $(".helado-info img").animate({
+                "margin-top": "-30px"
+            },"slow");
+            $(".arrows .fa-angle-down").css("display","block");
+            $(".arrows .fa-angle-up").css("display","none");
+        }
+    });
     
     
 })
