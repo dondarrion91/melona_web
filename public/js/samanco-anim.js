451024f74a574b9a6231e4bf96bee9d0b6a2228e
Julian <julisanchez1991@gmail.com>
2020-01-25 02:36:32 -0300

    25/1/2020

--- public/js/samanco-anim.js
+++ public/js/samanco-anim.js
@@ -1,39 +1,77 @@
-$(document).ready(function(){
-    console.log($(".tarjeta #parrafo-samanco").css("display"));
+var width = $("body").width();
 
+
+$(document).ready(function(){
+    console.log(width);
+
+    //segundo carrousel
     
-    $(".boton #btn-up").css("display","none");
-    $(".boton").click(function(){
-        if($(".tarjeta #parrafo-samanco").css("display") == "none"){
-        
-            $(".tarjeta #parrafo-samanco").slideDown("slow");
-            $(".boton #btn-down").css("display","none");
-            $(".boton #btn-up").css("display","block");
+    $(".boton1 #btn-up").css("display","none");
+    $(".boton2 #btn-up").css("display","none");
+    $(".boton1").click(function(){
+        if($(".tarjeta #parrafo-samanco-0").css("display") == "none"){
+            
+            $(".tarjeta #parrafo-samanco-0").slideDown("slow");
+            $(".boton1 #btn-down").css("display","none");
+            $(".boton1 #btn-up").css("display","block");
             console.log($(".tarjeta #parrafo-samanco").css("display"));
         
-        }else if ($(".tarjeta #parrafo-samanco").css("display") == "block"){
+        }else if ($(".tarjeta p").css("display") == "block"){
             console.log("works");
-            $(".tarjeta #parrafo-samanco").slideUp("slow");
-            $(".boton #btn-down").css("display","block");
-            $(".boton #btn-up").css("display","none");
+            $(".tarjeta #parrafo-samanco-0").slideUp("slow");
+            $(".boton1 #btn-down").css("display","block");
+            $(".boton1 #btn-up").css("display","none");
         }
     });
+
+    $(".boton2").click(function(){
+        if($(".tarjeta #parrafo-samanco-1").css("display") == "none"){
+        
+            $(".tarjeta #parrafo-samanco-1").slideDown("slow");
+            $(".boton2 #btn-down").css("display","none");
+            $(".boton2 #btn-up").css("display","block");
+            console.log($(".tarjeta #parrafo-samanco").css("display"));
+        
+        }else if ($(".tarjeta #parrafo-samanco-1").css("display") == "block"){
+            console.log("works");
+            $(".tarjeta #parrafo-samanco-1").slideUp("slow");
+            $(".boton2 #btn-down").css("display","block");
+            $(".boton2 #btn-up").css("display","none");
+        }
+    });
+
+    //primer carrousel
+
 
     $(".arrows .fa-angle-up").css("display","none");
     $(".arrows").click(function(){
         if($(".helado-info-body p").css("display") == "none"){
         
             $(".helado-info-body p").slideDown("slow");
-            $(".helado-info").animate({
+
+            if(width>425){
+                $(".helado-info").animate({
+                    width: '80%'
+                },"slow");
+            }else if(width<=425){
                 
-                width: '90%'
-            },"slow");
+                $(".helado-info").animate({
+                    width: '100%'
+                },"slow");
+            }
+
+            
+
             $(".helado-info-body p").animate({
-                "font-size": "14px"
+                "font-size": "11px"
             },"slow");
+
+
             $(".helado-info img").animate({
                 "margin-top": "0px"
             },"slow");
+
+
             $(".arrows .fa-angle-down").css("display","none");
             $(".arrows .fa-angle-up").css("display","block");
             
@@ -44,12 +82,16 @@ $(document).ready(function(){
             $(".helado-info").animate({
                 width: '40%'
             },"slow");
+
+            
             $(".helado-info-body p").animate({
                 "font-size": "0px"
             },"slow");
+
             $(".helado-info img").animate({
                 "margin-top": "-30px"
             },"slow");
+            
             $(".arrows .fa-angle-down").css("display","block");
             $(".arrows .fa-angle-up").css("display","none");
         }
